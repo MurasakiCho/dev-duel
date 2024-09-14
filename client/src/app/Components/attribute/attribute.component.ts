90e4b42d394cb02aabbb82249819ea995a622107
Sylrelessa <briana.oneal823@gmail.com>
2024-09-13 20:54:33 -0400

    set up dynamic values in components

--- client/src/app/Components/attribute/attribute.component.ts
+++ client/src/app/Components/attribute/attribute.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-attribute',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attribute.component.css']
 })
 export class AttributeComponent implements OnInit {
+
+  @Input() attributeName = ""
+  @Input() attributeValue =""
 
   constructor() { }
 
--- client/src/app/Components/profile/profile.component.ts
+++ client/src/app/Components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  @Input() imgSrc: string = ""
+  @Input() bio: string = ""
+  
   constructor() { }
 
   ngOnInit(): void {
